34f5cfb973d5e21c368a4300dc5cc27798f5e3c3
onlychoice <jzhihui521@gmail.com>
2011-11-17 06:21:52 +0000

    #misc: 重构

--- src/main/java/com/netease/xmpp/robot/RobotUpdateCompletedEventHandler.java
+++ src/main/java/com/netease/xmpp/robot/RobotUpdateCompletedEventHandler.java
@@ -21,8 +21,7 @@ public class RobotUpdateCompletedEventHandler extends UpdateCompletedEventHandle
                 Robot.getInstance().start();
             }
 
-            // TODO thread numbers
-            TaskExecutor.getInstance(1).resume();
+            TaskExecutor.getInstance().resume();
         }
     }
 }
